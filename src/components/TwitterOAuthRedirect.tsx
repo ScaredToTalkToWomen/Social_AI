30b49c6c4d5e7176604843066cf60a436651d157
Fun Zheng Bin <funbin48@gmail.com>
2025-11-04 23:44:43 +0800

    Updated TwitterOAuthRedirect.tsx

--- src/components/TwitterOAuthRedirect.tsx
+++ src/components/TwitterOAuthRedirect.tsx
@@ -36,7 +36,7 @@ export function TwitterOAuthRedirect() {
 
       setMessage('Connecting to Twitter...');
 
-      const response = await fetch('https://zhengbin.app.n8n.cloud/webhook-test/x-login', {
+      const response = await fetch('https://zhengbin.app.n8n.cloud/webhook/x-login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
